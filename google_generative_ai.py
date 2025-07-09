09d047e28138c87bdf288f635486dbf9a67e2ac0
mianmian7 <45864491+mianmian7@users.noreply.github.com>
2025-07-09 10:48:11 +0800

    Update google_generative_ai.py
    修改模型为最新gemini-2.5-pro

--- google_generative_ai.py
+++ google_generative_ai.py
@@ -22,7 +22,7 @@ class GoogleGenerativeAI:
             {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"}
         ]
 
-        self.model = genai.GenerativeModel(model_name="gemini-pro",
+        self.model = genai.GenerativeModel(model_name="gemini-2.5-pro",
                                            generation_config=generation_config,
                                            safety_settings=safety_settings)
 
